df677310e14d76da9336f75b2e529b3e9621bfe6
klucas33 <klucas96@naver.com>
2022-06-27 23:00:34 +0900

    typescript

---
+++ src/routes/Chart.tsx
@@ -0,0 +1,74 @@
+import { useQuery } from "react-query";
+import { fetchCoinHistory } from "../api";
+import ApexChart from "react-apexcharts";
+
+interface IHistorical {
+  time_open: string;
+  time_close: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap: number;
+}
+
+interface CharProps {
+  coinId: string;
+}
+
+function Chart({ coinId }: CharProps) {
+  const { isLoading, data } = useQuery<IHistorical[]>(
+    ["ohlcv", coinId],
+    () => fetchCoinHistory(coinId),
+    {
+      refetchInterval: 10000,
+    }
+  );
+  return (
+    <div>
+      {isLoading ? (
+        "Loading chart..."
+      ) : (
+        <ApexChart
+          type="line"
+          series={[
+            {
+              name: "Price",
+              data: data?.map((price) => price.close),
+            },
+          ]}
+          options={{
+            theme: { mode: "dark" },
+            chart: {
+              height: 300,
+              width: 500,
+              toolbar: { show: false },
+              background: "transparent",
+            },
+            stroke: { curve: "smooth", width: 5 },
+            grid: { show: false },
+            yaxis: { show: false },
+            xaxis: {
+              labels: { show: false },
+              axisTicks: { show: false },
+              axisBorder: { show: false },
+              type: "datetime",
+              categories: data?.map((price) => price.time_close),
+            },
+            fill: {
+              type: "gradient",
+              gradient: { gradientToColors: ["#00b894"], stops: [0, 100] },
+            },
+            colors: ["#0984e3"],
+            tooltip: {
+              y: { formatter: (value) => `$${value.toFixed(2)}` },
+            },
+          }}
+        />
+      )}
+    </div>
+  );
+}
+
+export default Chart;
