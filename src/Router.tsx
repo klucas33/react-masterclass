d881755f8e8e8e0561df1ea9d61322a309476a6c
klucas33 <klucas96@naver.com>
2022-06-27 23:35:50 +0900

    godd

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,15 +1,15 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Coin from "./routes/Coin";
 import Coins from "./routes/Coins";
 
 function Router() {
   return (
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/:coinId/*" element={<Coin />} />
         <Route path="/" element={<Coins />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 export default Router;
