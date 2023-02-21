2613ead7f8b58b4446e560a42abf8a63f337172c
Lucas <klucas96@naver.com>
2023-02-21 16:21:08 +0900

    added

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,9 +2,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Coin from "./routes/Coin";
 import Coins from "./routes/Coins";
 
+const BASENAME = "https://klucas33.github.io/react-masterclass";
+
 function Router() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={BASENAME}>
       <Routes>
         <Route path="/:coinId/*" element={<Coin />} />
         <Route path="/" element={<Coins />} />
